e15800f85fbfb204921de35b4d99de6e680429e4
Marcos Damas <marcosdamas12@gmail.com>
2021-02-24 20:49:32 -0400

    configuracion Angular

---
+++ controllers/main.js
@@ -0,0 +1,35 @@
+var app = angular.module("app",[]);
+
+app.controller("mainController", function($scope,$http){
+    $scope.tittle="prueba ";
+
+
+    $scope.sendmail = function(){
+        
+        //alert($scope.name+" "+$scope.email+" "+$scope.phone);
+        //console.log($scope.username);
+    
+           $http({
+             method: 'POST',
+             //url: 'http://localhost:3000/emails/', 
+             url: 'http://mdprofessionalservice.com:3000//emails/',
+             data:{
+                   name:$scope.name,
+                   email:$scope.email,
+                   phone: $scope.phone
+                  }
+            //headers:{'Content-Type': 'application/x-www-form-urlencoded'},
+          }).then(function(response) {
+            swal({
+                title: "Genial!",
+                text: "Tu Email ha sido enviado!",
+                icon: "success",
+                button: "OK",
+                dangerMode: true
+              });
+     ;
+          });
+   
+        }
+
+});
